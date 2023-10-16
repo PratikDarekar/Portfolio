f8cd399240a2cc4225c7712d16c729816b768e30
PratikDarekar <140389150+PratikDarekar@users.noreply.github.com>
2023-10-16 12:49:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        // checks if window is scrolled more than 500px, adds/removes solid class
+        if ($(this).scrollTop() > 550) {
+            $('.navbar').addClass('solid');
+            $('.back-to-top').addClass('visible');
+        } else {
+            $('.navbar').removeClass('solid');
+            $('.back-to-top').removeClass('visible');
+        }
+
+    });
+});
+
+$(document).ready(function () {
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+});
+
+/* Download CV Button*/
+
+const downloadButton = document.getElementById("download-cv");
+downloadButton.addEventListener("click", () => {
+  // Replace 'your-pdf-url.pdf' with the actual URL of your PDF file
+  const pdfUrl = 'Resume/NEWCV (2).pdf';
+  const link = document.createElement("a");
+  link.href = pdfUrl;
+  link.download = "Resume/NEWCV (2).pdf"; // Specify the desired file name
+  link.click();
+});
